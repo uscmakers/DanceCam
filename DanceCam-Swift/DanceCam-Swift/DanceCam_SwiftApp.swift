a21cf0b954c36f166e98d8740323419ac6f093ff
Abby Farhat <abbysfarhat@gmail.com>
2024-10-07 20:54:42 -0700

    chore: swift app setup

---
+++ DanceCam-Swift/DanceCam-Swift/DanceCam_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanceCam_SwiftApp.swift
+//  DanceCam-Swift
+//
+//  Created by Abby Farhat on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DanceCam_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DanceCam-Swift/DanceCam-SwiftTests/DanceCam_SwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  DanceCam_SwiftTests.swift
+//  DanceCam-SwiftTests
+//
+//  Created by Abby Farhat on 10/7/24.
+//
+
+import Testing
+@testable import DanceCam_Swift
+
+struct DanceCam_SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
