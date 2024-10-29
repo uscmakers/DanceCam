7df5f15d695f21cb9eab80692d60644d82931a37
Austin Tsai <aytsai@usc.edu>
2024-10-28 21:30:58 -0700

    works locally

--- FlaskServer/server.py
+++ FlaskServer/server.py
@@ -1,23 +1,16 @@
 from flask import Flask, request, jsonify
+import pathlib
 
+# Define Flask server
 app = Flask(__name__)
+thisdir = pathlib.Path(__file__).parent.absolute() # path to directory of this file
 
-# route to get data 
-@app.route('/')
-def get_data():
-    # dummy response
-    return jsonify({"status": "success", "data": "Hello from Raspberry Pi"})
-
-
-# route to retrieve data
-@app.route("/api/post", methods=["POST"])
-def receive_data():
-    data = request.json
-    # process received data here
-    print("Received data!")
-    print(data)
-    return jsonify({"status": "received"})
+# Move motor route
+@app.route('/move', methods=['POST'])
+def move_one():
+    res = jsonify({})
+    res.status_code = 201 # Status code for "created"
+    return res
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) # ip address and port 
-    
+    app.run(host='0.0.0.0', port=4444)
