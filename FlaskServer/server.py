16cc9ef8d567fc1270ee9189655fd38aa2ff5420
Kenny Nguyen <kennyang@usc.edu>
2024-10-28 21:02:15 -0700

    fixed routes

--- FlaskServer/server.py
+++ FlaskServer/server.py
@@ -2,18 +2,21 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# route to get data 
-@app.route('/api/data', methods=['GET'])
+
+# route to get data
+@app.route("/api/get", methods=["GET"])
 def get_data():
     # dummy response
     return jsonify({"status": "success", "data": "Hello from Raspberry Pi"})
 
-# route to retrieve data 
-@app.route('/api/data', methods=['POST'])
+
+# route to retrieve data
+@app.route("/api/post", methods=["POST"])
 def receive_data():
     data = request.json
     # process received data here
     return jsonify({"status": "received"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) # ip address and port 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # ip address and port
