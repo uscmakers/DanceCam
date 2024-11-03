f2b456e91bfc4b86c0c4934e85ebaf46c34f2dd3
Irith Katiyar <imkatiya@usc.edu>
2024-11-03 14:45:22 -0800

    Updated test client

--- client.py
+++ client.py
@@ -7,6 +7,6 @@ payload = {'text': 'The beauty of the world is greatly dependent on the eyes see
 # Defining content type for our payload
 headers = {'Content-type': 'application/json'}
 # Sending a post request to the server (API)
-response = requests.post(url="http://192.168.0.102:5000/", data=json.dumps(payload), headers=headers)
+response = requests.post(url="http://0.0.0.0:5000/", data=json.dumps(payload), headers=headers)
 # Printing out the response of API
 print(response.text)
