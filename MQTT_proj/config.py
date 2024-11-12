c2864933611e7fb697fa768ab16b0865e203188a
Irith Katiyar <imkatiya@usc.edu>
2024-11-11 20:29:46 -0800

    Delete MQTT_proj directory

--- MQTT_proj/config.py
+++
@@ -1,9 +0,0 @@
-MQTT_CONFIG = {
-    "broker": "localhost",
-    "port": 1883,
-    "topics": {
-        "sensors": "sensors/#",
-        "temperature": "sensors/temperature",
-        "humidity": "sensors/humidity",
-    },
-}
