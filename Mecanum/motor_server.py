5db36382b44c52b4076dd319e992d563a275c22c
atsai488 <144174796+atsai488@users.noreply.github.com>
2024-11-11 20:25:03 -0800

    Create motor_server.py

---
+++ Mecanum/motor_server.py
@@ -0,0 +1,85 @@
+# Importing required packages
+import sys
+import os
+
+# Import all functions form Motor.py
+from Motor import *
+
+# Importing flask stuff
+from flask import Flask, request, jsonify
+
+# Initiating a Flask application
+app = Flask(__name__)
+
+"""
+    Simple end point for testing functionality 
+"""
+
+
+@app.route(rule="/", methods=["GET", "POST"])
+def handle_request():
+    # The GET endpoint
+    if request.method == "GET":
+        return "This is the GET Endpoint of flask API."
+
+    # The POST endpoint
+    if request.method == "POST":
+        # accessing the passed payload
+        payload = request.get_json()
+        # capitalizing the text
+        cap_text = payload["text"].upper()
+        # Creating a proper response
+        response = {"cap-text": cap_text}
+        # return the response as JSON
+        return jsonify(response)
+
+
+"""
+    Endpoint for sending signal to motors
+"""
+
+
+@app.route(rule="/move", methods=["POST"])
+def handle_move_request():
+    PWM = Motor()
+
+    data = request.get_json()
+
+    # make sure there are 4 numbers
+    if not all(key in data for key in ("duty1", "duty2", "duty3", "duty4")):
+        return jsonify({"error": "Missing one or more required numbers"}), 400
+
+    # extract numbers and save
+    try:
+        x1 = data["duty1"]
+        x2 = data["duty2"]
+        x3 = data["duty3"]
+        x4 = data["duty4"]
+    except ValueError:
+        return jsonify({"error": "All values must be numbers"}), 400
+
+    PWM.setMotorModel(x1, x2, x3, x4)
+
+    return jsonify({"status": "Successfully triggered motors"})
+
+
+"""
+    Endpoint for stopping all motors
+"""
+
+
+@app.route(rule="/stop", methods=["POST"])
+def handle_stop_request():
+    PWM = Motor()
+
+    # set all motors to 0, stops all movement
+    PWM.setMotorModel(0, 0, 0, 0)
+
+    # successful message
+    return jsonify({"status": "Successfully stopped all motors"})
+
+
+# Running the API
+if __name__ == "__main__":
+    # Setting host = "0.0.0.0" runs it on localhost
+    app.run(host="0.0.0.0", port=8000, debug=True)
